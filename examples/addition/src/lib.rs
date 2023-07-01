66c293f6f000534e6b9cbbeb64f0066fc4f5dadc
clearloop <26088946+clearloop@users.noreply.github.com>
2023-07-01 17:04:26 +0800

    feat(zink): introduce panic handler

--- examples/addition/src/lib.rs
+++ examples/addition/src/lib.rs
@@ -1,13 +1,11 @@
 //! Addition example.
 #![no_std]
 
+// for panic handler.
+extern crate zink;
+
 /// Adds two numbers together.
 #[no_mangle]
 pub extern "C" fn add(x: u64, y: u64) -> u64 {
     x + y
-}
-
-#[panic_handler]
-fn panic(_info: &core::panic::PanicInfo) -> ! {
-    loop {}
 }
---
+++ zink/src/lib.rs
@@ -0,0 +1,6 @@
+#![no_std]
+
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
