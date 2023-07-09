4b3c82b47379aaf23725abd3b3669ba3cf32c0f0
clearloop <26088946+clearloop@users.noreply.github.com>
2023-07-09 08:38:01 +0800

    chore(compiler): addition tests

--- compiler/tests/add.rs
+++ compiler/tests/add.rs
@@ -9,7 +9,7 @@ mod common;
 #[test]
 fn i32_add() -> Result<()> {
     let wasm = common::load("i32add", "params")?;
-    let bytecode = Compiler::compile(&wasm);
+    let bytecode = Compiler::compile(&wasm)?;
 
     assert_eq!(hex::encode(bytecode), "6000356020350160005260206000f3");
     Ok(())
