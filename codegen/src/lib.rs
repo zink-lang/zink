3765ce492b5a0f950f2d6d54daf4cd1c59731110
clearloop <26088946+clearloop@users.noreply.github.com>
2023-07-15 00:32:17 +0800

    chore(codegen): rename patch to relocate

--- codegen/src/lib.rs
+++ codegen/src/lib.rs
@@ -12,7 +12,6 @@ pub use crate::{
     masm::MacroAssembler,
     result::{Error, Result},
 };
-use opcodes::ShangHai as OpCode;
 use smallvec::SmallVec;
 
 pub mod abi;
@@ -35,50 +34,3 @@ pub type Buffer = SmallVec<[u8; BUFFER_LIMIT]>;
 /// Solidity's implementation uses 16 slots for locals.
 /// ref: <https://docs.soliditylang.org/en/v0.8.20/internals/optimizer.html#stackcompressor>
 pub type Locals = SmallVec<[LocalSlot; 16]>;
-
-/// Patch program counter to buffer.
-pub fn patch(buffer: &mut Buffer, original_pc: usize, target_pc: usize) -> Result<usize> {
-    let mut pc = target_pc;
-    let mut new_buffer: Buffer = buffer[..original_pc].into();
-    let rest_buffer: Buffer = buffer[original_pc..].into();
-
-    // Update the target program counter
-    {
-        // The maximum size of the PC is 2 bytes, whatever PUSH1 or PUSH2
-        // takes 1 more byte.
-        pc += 1;
-
-        // Update the program counter for the edge cases.
-        //
-        // Start from 0xff, the lowest significant bytes of the target
-        // program counter will take 2 bytes instead of 1 byte.
-        //
-        // | PC   | PC BYTES | TARGET PC |
-        // |------|----------|-----------|
-        // | 0xfe | 1        |      0xff |
-        // | 0xff | 2        |     0x101 |
-        pc += if pc > 0xfe {
-            new_buffer.push(OpCode::PUSH2.into());
-            2
-        } else {
-            new_buffer.push(OpCode::PUSH1.into());
-            1
-        }
-    }
-
-    // Check PC range.
-    if pc > BUFFER_LIMIT {
-        return Err(Error::InvalidPC(pc));
-    }
-
-    new_buffer.extend_from_slice(&pc.to_ls_bytes());
-    new_buffer.extend_from_slice(&rest_buffer);
-
-    // Check buffer size.
-    if new_buffer.len() > BUFFER_LIMIT {
-        return Err(Error::BufferOverflow(new_buffer.len()));
-    }
-
-    *buffer = new_buffer;
-    Ok(pc)
-}
