5a32b352c2a6efbbc10a4aa5e7584c604b34e452
clearloop <clearloop@users.noreply.github.com>
2023-08-03 09:10:56 +0000

    deploy: 80ded1fa9eb26b8b90227247d1e14fe0a711bf0a

--- rustdocs/implementors/clap_builder/derive/trait.Args.js
+++ rustdocs/implementors/clap_builder/derive/trait.Args.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {
 "elko":[["impl Args for <a class=\"struct\" href=\"elko/struct.Elko.html\" title=\"struct elko::Elko\">Elko</a>"]],
 "zinkc":[["impl Args for <a class=\"struct\" href=\"zinkc/struct.Zinkc.html\" title=\"struct zinkc::Zinkc\">Zinkc</a>"]],
-"zinkup":[["impl Args for <a class=\"struct\" href=\"zinkup/build/struct.Build.html\" title=\"struct zinkup::build::Build\">Build</a>"],["impl Args for <a class=\"struct\" href=\"zinkup/compile/struct.Compile.html\" title=\"struct zinkup::compile::Compile\">Compile</a>"],["impl Args for <a class=\"struct\" href=\"zinkup/new/struct.New.html\" title=\"struct zinkup::new::New\">New</a>"]]
+"zinkup":[["impl Args for <a class=\"struct\" href=\"zinkup/new/struct.New.html\" title=\"struct zinkup::new::New\">New</a>"],["impl Args for <a class=\"struct\" href=\"zinkup/compile/struct.Compile.html\" title=\"struct zinkup::compile::Compile\">Compile</a>"],["impl Args for <a class=\"struct\" href=\"zinkup/build/struct.Build.html\" title=\"struct zinkup::build::Build\">Build</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- rustdocs/implementors/clap_builder/derive/trait.CommandFactory.js
+++ rustdocs/implementors/clap_builder/derive/trait.CommandFactory.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {
 "elko":[["impl CommandFactory for <a class=\"struct\" href=\"elko/struct.Elko.html\" title=\"struct elko::Elko\">Elko</a>"]],
 "zinkc":[["impl CommandFactory for <a class=\"struct\" href=\"zinkc/struct.Zinkc.html\" title=\"struct zinkc::Zinkc\">Zinkc</a>"]],
-"zinkup":[["impl CommandFactory for <a class=\"struct\" href=\"zinkup/build/struct.Build.html\" title=\"struct zinkup::build::Build\">Build</a>"],["impl CommandFactory for <a class=\"struct\" href=\"zinkup/new/struct.New.html\" title=\"struct zinkup::new::New\">New</a>"],["impl CommandFactory for <a class=\"struct\" href=\"zinkup/compile/struct.Compile.html\" title=\"struct zinkup::compile::Compile\">Compile</a>"]]
+"zinkup":[["impl CommandFactory for <a class=\"struct\" href=\"zinkup/compile/struct.Compile.html\" title=\"struct zinkup::compile::Compile\">Compile</a>"],["impl CommandFactory for <a class=\"struct\" href=\"zinkup/new/struct.New.html\" title=\"struct zinkup::new::New\">New</a>"],["impl CommandFactory for <a class=\"struct\" href=\"zinkup/build/struct.Build.html\" title=\"struct zinkup::build::Build\">Build</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
