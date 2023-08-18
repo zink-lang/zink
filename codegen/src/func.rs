9a6e99f59182a62b8b6ca9a9b9c59bd29e16df34
clearloop <26088946+clearloop@users.noreply.github.com>
2023-08-19 00:16:58 +0400

    feat(codegen): introduce built-in function sload and sstore

--- codegen/src/func.rs
+++ codegen/src/func.rs
@@ -1,7 +1,7 @@
 //! Built-in functions for EVM
 use opcodes::ShangHai as OpCode;
 
-/// Function `select`.
+/// Function `select` from WASM which is not avaiable in EVM.
 const SELECT: [OpCode; 6] = [
     OpCode::JUMPDEST,
     OpCode::POP,
@@ -11,21 +11,57 @@ const SELECT: [OpCode; 6] = [
     OpCode::JUMP,
 ];
 
+/// Function `sload` from EVM which is not avaiable in WASM.
+const SLOAD: [OpCode; 5] = [
+    OpCode::JUMPDEST,
+    OpCode::SLOAD,
+    OpCode::Data(0x06),
+    OpCode::ADD,
+    OpCode::JUMP,
+];
+
+/// Function `sload` from EVM which is not avaiable in WASM.
+const SSTORE: [OpCode; 5] = [
+    OpCode::JUMPDEST,
+    OpCode::SSTORE,
+    OpCode::Data(0x06),
+    OpCode::ADD,
+    OpCode::JUMP,
+];
+
 /// Function selector.
 #[derive(Clone, Copy, Debug, PartialOrd, Ord, PartialEq, Eq, Hash)]
 pub enum Func {
-    /// Run select.
+    /// Run function select.
     Select,
+    /// Run function sload.
+    Sload,
+    /// Run function sstore.
+    Sstore,
 }
 
 impl Func {
     /// Get the bytecode of the function.
     pub fn bytecode(&self) -> Vec<u8> {
         match self {
-            Self::Select => SELECT,
+            Self::Select => SELECT.to_vec(),
+            Self::Sload => SLOAD.to_vec(),
+            Self::Sstore => SSTORE.to_vec(),
         }
         .into_iter()
         .map(|op| op.into())
         .collect()
+    }
+}
+
+impl TryFrom<(&str, &str)> for Func {
+    type Error = ();
+
+    fn try_from(import: (&str, &str)) -> Result<Self, Self::Error> {
+        match import {
+            ("zink", "sload") => Ok(Self::Sload),
+            ("zink", "sstore") => Ok(Self::Sstore),
+            _ => Err(()),
+        }
     }
 }
