60e2bf1fe702b6d3375ed48b8ff48f87560791c8
clearloop <26088946+clearloop@users.noreply.github.com>
2023-08-22 00:42:08 +0400

    ci(typos): checkout branch before checking

--- zink/src/lib.rs
+++ zink/src/lib.rs
@@ -7,6 +7,11 @@ pub mod storage {
     pub use super::imports::{sload, sstore};
 }
 
+/// Event interfaces
+pub mod events {
+    pub use super::imports::{log0, log1, log2, log3, log4};
+}
+
 // Panic hook implementation
 #[cfg(not(test))]
 #[panic_handler]
