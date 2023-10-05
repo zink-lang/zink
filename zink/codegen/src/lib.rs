269f501e37e96746da67f46e6fa2c827f611c53b
clearloop <26088946+clearloop@users.noreply.github.com>
2023-10-05 19:26:38 +0800

    docs(zink-codegen): introduce example for the event interface

--- zink/codegen/src/lib.rs
+++ zink/codegen/src/lib.rs
@@ -6,6 +6,29 @@ use syn::{parse_macro_input, DeriveInput};
 mod event;
 
 /// Event logging interface
+///
+/// ```rust
+/// use zink::Event;
+///
+/// /// A `Ping` event.
+/// #[derive(Event)]
+/// struct Ping;
+///
+/// #[no_mangle]
+/// pub extern "C" fn log0() {
+///     Ping.log0();
+/// }
+/// ```
+///
+/// will generate:
+///
+/// ```rust
+/// struct Ping;
+///
+/// impl zink::Event for Ping {
+///     const NAME: &'static [u8] = b"Ping";
+/// }
+/// ```
 #[proc_macro_derive(Event)]
 pub fn event(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
