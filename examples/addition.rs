572d08ccc400eb6c72bd8f00ed8d1f4d378c6499
clearloop <26088946+clearloop@users.noreply.github.com>
2023-10-28 05:34:18 +0800

    docs(zink): addition example in README

--- examples/addition.rs
+++ examples/addition.rs
@@ -11,3 +11,18 @@ pub fn addition(x: u64, y: u64) -> u64 {
 
 #[cfg(not(target_arch = "wasm32"))]
 fn main() {}
+
+#[test]
+fn test() -> anyhow::Result<()> {
+    use zint::{Bytes32, Contract};
+    let mut contract = Contract::search("addition")?.compile()?;
+
+    let info = contract.execute([
+        "addition(u64,u64)".as_bytes(),
+        &1u64.to_bytes32(),
+        &2u64.to_bytes32(),
+    ])?;
+    assert_eq!(info.ret, 3u64.to_bytes32());
+
+    Ok(())
+}
