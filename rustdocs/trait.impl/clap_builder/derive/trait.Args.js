6349a2a3e855155ea0e5867f38ff5bc95e2eea1e
clearloop <clearloop@users.noreply.github.com>
2023-12-15 18:18:21 +0000

    deploy: 5014cbd2992b3d8f139bf760532e7454b000fb75

--- rustdocs/trait.impl/clap_builder/derive/trait.Args.js
+++ rustdocs/trait.impl/clap_builder/derive/trait.Args.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {
-"conta":[["impl Args for <a class=\"struct\" href=\"conta/cmd/struct.Conta.html\" title=\"struct conta::cmd::Conta\">Conta</a>"],["impl Args for <a class=\"struct\" href=\"conta/cmd/bump/struct.Bump.html\" title=\"struct conta::cmd::bump::Bump\">Bump</a>"],["impl Args for <a class=\"struct\" href=\"conta/cmd/publish/struct.Publish.html\" title=\"struct conta::cmd::publish::Publish\">Publish</a>"]],
+"conta":[["impl Args for <a class=\"struct\" href=\"conta/cmd/publish/struct.Publish.html\" title=\"struct conta::cmd::publish::Publish\">Publish</a>"],["impl Args for <a class=\"struct\" href=\"conta/cmd/bump/struct.Bump.html\" title=\"struct conta::cmd::bump::Bump\">Bump</a>"],["impl Args for <a class=\"struct\" href=\"conta/cmd/struct.Conta.html\" title=\"struct conta::cmd::Conta\">Conta</a>"]],
 "elko":[["impl Args for <a class=\"struct\" href=\"elko/struct.Elko.html\" title=\"struct elko::Elko\">Elko</a>"]],
 "zinkc":[["impl Args for <a class=\"struct\" href=\"zinkc/struct.Zinkc.html\" title=\"struct zinkc::Zinkc\">Zinkc</a>"]],
 "zinkup":[["impl Args for <a class=\"struct\" href=\"zinkup/compile/struct.Compile.html\" title=\"struct zinkup::compile::Compile\">Compile</a>"]]
