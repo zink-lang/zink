8603d31ff31bc35276e2dc82a76d151e12ab2eba
clearloop <clearloop@users.noreply.github.com>
2023-12-23 04:07:46 +0000

    deploy: dda46c5591093ee189d55b278ca49346a0b6619c

--- rustdocs/trait.impl/clap_builder/derive/trait.FromArgMatches.js
+++ rustdocs/trait.impl/clap_builder/derive/trait.FromArgMatches.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
-"elko":[["impl FromArgMatches for <a class=\"struct\" href=\"elko/new/struct.New.html\" title=\"struct elko::new::New\">New</a>"],["impl FromArgMatches for <a class=\"struct\" href=\"elko/build/struct.Build.html\" title=\"struct elko::build::Build\">Build</a>"]],
+"elko":[["impl FromArgMatches for <a class=\"struct\" href=\"elko/build/struct.Build.html\" title=\"struct elko::build::Build\">Build</a>"],["impl FromArgMatches for <a class=\"struct\" href=\"elko/new/struct.New.html\" title=\"struct elko::new::New\">New</a>"]],
 "zinkc":[["impl FromArgMatches for <a class=\"struct\" href=\"zinkc/cli/struct.Compile.html\" title=\"struct zinkc::cli::Compile\">Compile</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
