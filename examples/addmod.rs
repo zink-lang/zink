b76e9b2d5ce0c4a8f5e6bf31c4a0ab26aa5183aa
malik <aremumalik05@gmail.com>
2024-11-28 10:32:35 +0100

    fix

--- examples/addmod.rs
+++ examples/addmod.rs
@@ -3,7 +3,7 @@
 #![cfg_attr(target_arch = "wasm32", no_main)]
 
 extern crate zink;
-use zink::{primitives::{numeric::Numeric, U256}, Asm};
+use zink::primitives::{numeric::Numeric, U256};
 
 #[zink::external]
 pub fn addmod_i32(a: i32, b: i32, n: i32) -> i32 {
