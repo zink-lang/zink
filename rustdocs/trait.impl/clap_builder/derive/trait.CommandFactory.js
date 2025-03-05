d020a6502dc763f45bf918fb15c7ac90bb0d947f
clearloop <clearloop@users.noreply.github.com>
2025-03-05 02:25:58 +0000

    deploy: ffc89d2bc186df5fa003e629410f59efe0b2304e

--- rustdocs/trait.impl/clap_builder/derive/trait.CommandFactory.js
+++ rustdocs/trait.impl/clap_builder/derive/trait.CommandFactory.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["elko",[["impl CommandFactory for <a class=\"struct\" href=\"elko/build/struct.Build.html\" title=\"struct elko::build::Build\">Build</a>"],["impl CommandFactory for <a class=\"struct\" href=\"elko/new/struct.New.html\" title=\"struct elko::new::New\">New</a>"]]],["zinkc",[["impl CommandFactory for <a class=\"struct\" href=\"zinkc/cli/struct.Compile.html\" title=\"struct zinkc::cli::Compile\">Compile</a>"],["impl CommandFactory for <a class=\"struct\" href=\"zinkc/config/struct.Config.html\" title=\"struct zinkc::config::Config\">Config</a>"]]]]);
+    var implementors = Object.fromEntries([["elko",[["impl CommandFactory for <a class=\"struct\" href=\"elko/struct.Build.html\" title=\"struct elko::Build\">Build</a>"],["impl CommandFactory for <a class=\"struct\" href=\"elko/struct.New.html\" title=\"struct elko::New\">New</a>"]]],["zinkc",[["impl CommandFactory for <a class=\"struct\" href=\"zinkc/cli/struct.Compile.html\" title=\"struct zinkc::cli::Compile\">Compile</a>"],["impl CommandFactory for <a class=\"struct\" href=\"zinkc/struct.Config.html\" title=\"struct zinkc::Config\">Config</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[264,287]}
+//{"start":57,"fragment_lengths":[242,272]}
--- rustdocs/trait.impl/wasmparser/readers/core/operators/trait.VisitOperator.js
+++ rustdocs/trait.impl/wasmparser/readers/core/operators/trait.VisitOperator.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["zingen",[["impl VisitOperator&lt;'_&gt; for <a class=\"struct\" href=\"zingen/codegen/function/struct.Function.html\" title=\"struct zingen::codegen::function::Function\">Function</a>"],["impl&lt;'a, T&gt; VisitOperator&lt;'a&gt; for <a class=\"struct\" href=\"zingen/validator/struct.ValidateThenVisit.html\" title=\"struct zingen::validator::ValidateThenVisit\">ValidateThenVisit</a>&lt;'_, T&gt;<div class=\"where\">where\n    T: VisitOperator&lt;'a, Output = Result&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.unit.html\">()</a>&gt;&gt;,</div>"]]]]);
+    var implementors = Object.fromEntries([["zingen",[["impl VisitOperator&lt;'_&gt; for <a class=\"struct\" href=\"zingen/struct.Function.html\" title=\"struct zingen::Function\">Function</a>"],["impl&lt;'a, T&gt; VisitOperator&lt;'a&gt; for <a class=\"struct\" href=\"zingen/validator/struct.ValidateThenVisit.html\" title=\"struct zingen::validator::ValidateThenVisit\">ValidateThenVisit</a>&lt;'_, T&gt;<div class=\"where\">where\n    T: VisitOperator&lt;'a, Output = Result&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.unit.html\">()</a>&gt;&gt;,</div>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[590]}
+//{"start":57,"fragment_lengths":[554]}
