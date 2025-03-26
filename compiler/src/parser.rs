cf7e467e3ee56b44c8e736b0ed24816aecea66cb
clearloop <tianyi.gc@gmail.com>
2025-03-26 16:36:22 +0800

    chore(compiler): ban duplicate functions with same logic

--- compiler/src/parser.rs
+++ compiler/src/parser.rs
@@ -105,6 +105,15 @@ impl<'p> Parser<'p> {
             index,
         })) = iter.next()
         {
+            if let Some(existing) = exports.get(&index) {
+                return Err(anyhow::anyhow!(
+                    "duplicate function: {name} and {existing} are sharing the same logic, \
+                    consider removing one of them, see https://github.com/zink-lang/zink/issues/319 \
+                    for more details."
+                )
+                .into());
+            }
+
             exports.insert(index, name.into());
         }
 
